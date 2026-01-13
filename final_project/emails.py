fced691e7ef948cb617877607b25b2fa04f37da0
Chonrachart <chonrachart.sieng@gmail.com>
2026-01-13 15:16:31 +0700

    adjust space

--- final_project/emails.py
+++ final_project/emails.py
@@ -29,12 +29,10 @@ def generate_email(sender, recipient, subject, body, attachment_path):
             subtype=mime_subtype,
             filename=os.path.basename(attachment_path)
         )
-
     return message
 
 def generate_email_without_attach(sender, recipient, subject, body):
     """Create email without an attachement"""
-
     # set information
     message = EmailMessage()
     message["From"] = sender
@@ -43,7 +41,6 @@ def generate_email_without_attach(sender, recipient, subject, body):
     message.set_content(body)
 
     return message
-
 
 def send_email(message):
     """send email"""
